fe0626cc9969425f8841cb1bd56d313bda1a819d
Tyler Baker <tyler@foundries.io>
2020-02-21 10:50:25 -0800

    merge in gcg changes
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 systemctl stop aktualizr-lite
 systemctl disable aktualizr-lite
 cp update.sh /sysroot/home/fio/update.sh
+cp DEMO_START /sysroot/home/fio/DEMO_START
+cp DEMO_STOP /sysroot/home/fio/DEMO_STOP
 chmod a+x /sysroot/home/fio/update.sh
+chmod a+x /sysroot/home/fio/DEMO_START
+chmod a+x /sysroot/home/fio/DEMO_STOP
 cp update.service /etc/systemd/system/update.service
 systemctl enable update.service
 aktualizr-lite update --update-name=11
--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ sleep 60
 
 echo "" | tee -a /dev/console
 echo "---------------------------------------------------------------" | tee -a /dev/console
-echo "UPDATER: Initialization sequence started" | tee -a /dev/console
+echo "UPDATER DEMO: Update initialization sequence started" | tee -a /dev/console
 echo "---------------------------------------------------------------" | tee -a /dev/console
 echo "" | tee -a /dev/console
 echo "---------------------------------------------------------------" | tee -a /dev/console
-echo "UPDATER: Checking update counter" | tee -a /dev/console
+echo "UPDATER DEMO: Checking update counter" | tee -a /dev/console
 echo "---------------------------------------------------------------" | tee -a /dev/console
 echo "" | tee -a /dev/console
 
@@ -16,82 +16,82 @@ counter=0
 
 echo "---------------------------------------------------------------" | tee -a /dev/console
 if test -f "/sysroot/home/fio/counter"; then
-	echo "UPDATER: Previous updates detected" | tee -a /dev/console
+	echo "UPDATER DEMO: Previous updates detected" | tee -a /dev/console
 	counter=$(cat /sysroot/home/fio/counter)
-	echo "UPDATER: Update count at $counter" | tee -a /dev/console
+	echo "UPDATER DEMO: Update count is $counter" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 else
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: No previous updates detected" | tee -a /dev/console
-	echo "UPDATER: Update count set to 0" | tee -a /dev/console
+	echo "UPDATER DEMO: No previous updates detected" | tee -a /dev/console
+	echo "UPDATER DEMO: Update count set to 0" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 fi
 
-sleep 20
+sleep 5
 
 active_image=$(aktualizr-lite status | grep 'Active image is:' | cut -d ':' -f 2 | cut -f 1 | xargs)
 echo "---------------------------------------------------------------" | tee -a /dev/console
-echo "UPDATER: The active image is: $active_image" | tee -a /dev/console
+echo "UPDATER DEMO: The active image is FoundriesFactory build: $active_image" | tee -a /dev/console
 echo "---------------------------------------------------------------" | tee -a /dev/console
 echo "" | tee -a /dev/console
 
 if test -f "/sysroot/home/fio/kver"; then
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: The Linux kernel version has changed:" | tee -a /dev/console
+	echo "UPDATER DEMO: The last update includes a new Linux kernel version:" | tee -a /dev/console
 	echo "$(cat /sysroot/home/fio/kver) ==> $(uname -r)" | tee -a /dev/console
 	echo $(uname -r) > /sysroot/home/fio/kver
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 else
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: The Linux kernel version is: $(uname -r)" | tee -a /dev/console
+	echo "UPDATER DEMO: The Linux kernel version is: $(uname -r)" | tee -a /dev/console
 	echo $(uname -r) > /sysroot/home/fio/kver
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 fi
 
-sleep 20
+sleep 5
 
 if [ "$active_image" = "11" ]; then
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: Downgrading to image: 8" | tee -a /dev/console
+	echo "UPDATER DEMO: Now Downgrading to FoundriesFactory build: 8" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 	aktualizr-lite update --update-name=8 | tee -a /dev/console
 	echo "" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: Image 8 has been applied successfully" | tee -a /dev/console
+	echo "UPDATER DEMO: Image 8 has been installed successfully" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
-	sleep 20
+	sleep 2
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: Rebooting device to apply update" | tee -a /dev/console
+	echo "UPDATER DEMO: Rebooting device to apply update" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 	counter=$((counter+1))
 	echo $counter > /sysroot/home/fio/counter
-	sleep 10
+	sleep 2
 	reboot
 else
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: Upgrading to image: 11" | tee -a /dev/console
+	echo "UPDATER DEMO: Now Upgrading to FoundriesFactory build: 11" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
 	aktualizr-lite update --update-name=11 | tee -a /dev/console
 	echo "" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-        echo "UPDATER: Image 11 has been applied successfully" | tee -a /dev/console
+        echo "UPDATER DEMO: Image 11 has been installed successfully" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
-	sleep 20
+	sleep 2
 	echo "---------------------------------------------------------------" | tee -a /dev/console
-	echo "UPDATER: Rebooting device to apply update" | tee -a /dev/console
+	echo "UPDATER DEMO: Rebooting device to apply update" | tee -a /dev/console
 	echo "---------------------------------------------------------------" | tee -a /dev/console
 	echo "" | tee -a /dev/console
         counter=$((counter+1))
         echo $counter > /sysroot/home/fio/counter
-	sleep 10
+	sleep 2
 	reboot
 fi
